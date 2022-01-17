be2619af17d1e85daac7f7dbd827ff57ba94783c
doombubbles <obsidiantoolzg@gmail.com>
2022-01-16 21:47:32 -0800

    Initial Commit

---
+++ IndiscriminatePetsMod.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Assets.Scripts.Data;
+using Assets.Scripts.Data.Cosmetics.Pets;
+using Assets.Scripts.Data.TrophyStore;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Extensions;
+using IndiscriminatePets;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(IndiscriminatePetsMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace IndiscriminatePets
+{
+    public class IndiscriminatePetsMod : BloonsTD6Mod
+    {
+        public override void OnTitleScreen()
+        {
+            foreach (var trophyStoreItem in GameData.Instance.trophyStoreItems.storeItems)
+            {
+                foreach (var trophyItemTypeData in trophyStoreItem.itemTypes.Where(data => data.itemType == TrophyItemType.TowerPet))
+                {
+                    if (trophyItemTypeData.itemTarget.IsType(out Pet pet))
+                    {
+                        pet.skinId = "";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,11 @@
+﻿namespace IndiscriminatePets
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.0.0";
+        public const string RepoOwner = "doombubbles";
+        public const string RepoName = "indiscriminate-pets";
+        public const string Name = "Indiscriminate Pets";
+        public const string Description = "Allows trophy store pets to apply to all skins of the hero they're for.";
+    }
+}
