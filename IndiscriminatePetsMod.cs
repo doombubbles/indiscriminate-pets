28275abed154cac55589017de9dc34e427098d4e
doombubbles <obsidiantoolzg@gmail.com>
2022-04-24 20:21:52 -0700

    File scoped namespaces

--- IndiscriminatePetsMod.cs
+++ IndiscriminatePetsMod.cs
@@ -10,20 +10,19 @@ using MelonLoader;
 [assembly: MelonInfo(typeof(IndiscriminatePetsMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
-namespace IndiscriminatePets
+namespace IndiscriminatePets;
+
+public class IndiscriminatePetsMod : BloonsTD6Mod
 {
-    public class IndiscriminatePetsMod : BloonsTD6Mod
+    public override void OnTitleScreen()
     {
-        public override void OnTitleScreen()
+        foreach (var trophyStoreItem in GameData.Instance.trophyStoreItems.storeItems)
         {
-            foreach (var trophyStoreItem in GameData.Instance.trophyStoreItems.storeItems)
+            foreach (var trophyItemTypeData in trophyStoreItem.itemTypes.Where(data => data.itemType == TrophyItemType.TowerPet))
             {
-                foreach (var trophyItemTypeData in trophyStoreItem.itemTypes.Where(data => data.itemType == TrophyItemType.TowerPet))
+                if (trophyItemTypeData.itemTarget.IsType(out Pet pet))
                 {
-                    if (trophyItemTypeData.itemTarget.IsType(out Pet pet))
-                    {
-                        pet.skinId = "";
-                    }
+                    pet.skinId = "";
                 }
             }
         }
--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,11 +1,10 @@
-﻿namespace IndiscriminatePets
+﻿namespace IndiscriminatePets;
+
+public static class ModHelperData
 {
-    public static class ModHelperData
-    {
-        public const string Version = "1.0.0";
-        public const string RepoOwner = "doombubbles";
-        public const string RepoName = "indiscriminate-pets";
-        public const string Name = "Indiscriminate Pets";
-        public const string Description = "Allows trophy store pets to apply to all skins of the hero they're for.";
-    }
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "doombubbles";
+    public const string RepoName = "indiscriminate-pets";
+    public const string Name = "Indiscriminate Pets";
+    public const string Description = "Allows trophy store pets to apply to all skins of the hero they're for.";
 }
